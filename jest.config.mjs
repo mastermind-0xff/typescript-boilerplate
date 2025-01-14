768c5daebcd994a47034755170decb53c277fd17
Tihomir Trifonov <t.a.trifonov@gmail.com>
2025-01-14 12:48:38 +0200

    jest config updated for e2e

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,7 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/__tests__/**/*.spec.ts', '<rootDir>/src/**/*.spec.ts'],
+  testMatch: ['<rootDir>/e2e/**/*.spec.ts', '<rootDir>/src/**/*.spec.ts'],
   testPathIgnorePatterns: ['/node_modules/'],
   coverageDirectory: './coverage',
   coveragePathIgnorePatterns: ['node_modules'],
